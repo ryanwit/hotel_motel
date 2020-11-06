9c22b127f565b323f7bb49f9395bee9f76de2456
Taylor <taylor.hackbart@du.edu>
2020-11-06 09:06:31 -0800

    creating partials and working on buttons

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,44 +1,45 @@
+var db = require("../models"); // Require in models folder
 
-var db = require("../models"); // Require in models folder 
-
-module.exports = function(app) {
-
-    app.get("/api/guests", function(req, res) {
-        db.Guest.findAll({}).then(function(result) {
-            console.log(result);
-            res.json(result)
-        });
+module.exports = function (app) {
+  app.get("/api/guests", function (req, res) {
+    db.Guest.findAll({}).then(function (result) {
+      console.log(result);
+      res.json(result);
     });
+  });
 
-    app.post("/api/guests", function(req, res) {
-        db.Guest.create({
-            name: req.body.name,
-            rooms: req.body.rooms,
-            phone: req.body.phone,
-            status: req.body.status
-        })
-        .then(function(result) {
-            res.json(result)
-              console.log(result)
-        });
-    });   
+  app.post("/api/guests", function (req, res) {
+    db.Guest.create({
+      name: req.body.name,
+      rooms: req.body.rooms,
+      phone: req.body.phone,
+      status: req.body.status,
+    }).then(function (result) {
+      res.json(result);
+      console.log(result);
+    });
+  });
 
-    app.put("/api/guests/:id", function(req, res) {
-        var condition = "id = " + req.params.id;
-      
-        console.log("condition", condition);
-      
-        guest.update({
-          status: req.body.status
-        }, condition, function(result) {
-          if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-          } else {
-            res.status(200).end();
-          }
-        });
-      });
-
-   
+  app.put("/api/guests/" + id, function (req, res) {
+    // var condition = "id = " + req.params.id;
+    var id = req.params.id;
+    // console.log("condition", condition);
+    db.Guest.update(
+      { status: req.body.status },
+      {
+      where: {
+        id: req.body.id ,
+      },
+    },
+      condition,
+      function (result) {
+        if (result.changedRows == 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      }
+    );
+  });
 };
