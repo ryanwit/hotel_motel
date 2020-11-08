b7c5dd188e4ac42c9be55a899998da1cfa668bb4
Dana Kuglin <dakuglin@gmail.com>
2020-11-08 15:02:49 -0700

    updated styling on logo and boxes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
 
   
   app.get("/", function(req, res) {
-    db.Guest.findAll({raw:true}).then(function(res2){
+    db.Guest.findAll({raw:true}).then(function(res2) {
       const dataset = { Guest: res2 }
       res.render("index", dataset )
     })
